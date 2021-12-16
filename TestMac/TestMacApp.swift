28a1d05a721cf79d78a0322b636eccb43db92c9e
Szabolcs Toth <szabolcs.toth@bitrise.io>
2021-12-16 21:22:49 +0000

    Initial Commit

---
+++ TestMac/TestMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMacApp.swift
+//  TestMac
+//
+//  Created by Szabolcs Toth on 16/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct TestMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
